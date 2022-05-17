4f510246213e85c895a1773c1ceabc2aa53696fe
Kyuhyeon Han <35136024+kahmnkk@users.noreply.github.com>
2022-05-17 20:50:18 +0900

    fix. update table

--- Assets/Scripts/DataTable/TableLoader_loadDatas.cs
+++ Assets/Scripts/DataTable/TableLoader_loadDatas.cs
@@ -6,77 +6,77 @@ using Utils;
 
 public partial class TableLoader : MonoSingleton<TableLoader>
 {
-	private IEnumerator LoadTableDataProcess()
-	{
-		_CompleteStep = 2;
+    private IEnumerator LoadTableDataProcess()
+    {
+        _CompleteStep = 2;
 
 #if UNITY_EDITOR
-		Load_table_skill_card("101_table_skill_card");
+        Load_table_skill_card("101_table_skill_card");
 #else
 		yield return Load_table_skill_card("101_table_skill_card");
 #endif
 
-		yield return Resources.UnloadUnusedAssets();
-		System.GC.Collect();
+        yield return Resources.UnloadUnusedAssets();
+        System.GC.Collect();
 
 
-		if (m_isLoadFail)
-			yield break;
+        if (m_isLoadFail)
+            yield break;
 
-		if (_tableHashs != null)
-			_tableHashs.Clear();
+        if (_tableHashs != null)
+            _tableHashs.Clear();
 
-		m_Clue = string.Empty;
+        m_Clue = string.Empty;
 
-		if(_CurrentStep < _CompleteStep)
-		{
-			_CurrentStep = _CompleteStep - 1;
+        if (_CurrentStep < _CompleteStep)
+        {
+            _CurrentStep = _CompleteStep - 1;
 
-			yield return null;
-			UpdateStep();
-		}
+            yield return null;
+            UpdateStep();
+        }
 
-		if (_CompleteEvent != null)
-		{
-			yield return null;
+        if (_CompleteEvent != null)
+        {
+            yield return null;
 
-			_CompleteEvent();
-			_CompleteEvent = null;
-		}
+            _CompleteEvent();
+            _CompleteEvent = null;
+        }
 
-		yield return null;
+        yield return null;
 
-		_CurrentStep = 0;
-		_CompleteStep = 0;
+        _CurrentStep = 0;
+        _CompleteStep = 0;
 
-		_UpdateEvent = null;
-	}
+        _UpdateEvent = null;
+    }
 
 #if UNITY_EDITOR
-	private void Load_table_skill_card(string fileName)
+    private void Load_table_skill_card(string fileName)
 #else
 	private IEnumerator Load_table_skill_card(string fileName)
 #endif
-	{
-		List<object> dataList = GetDataList(fileName);
-		if (dataList == null)
-		{
-			Debug.Log(GetType().Name + " Load_table_skill_card() : data list is null" + fileName);
-		}
-		else
-		{
-			for (int i = 0, count = dataList.Count; i < count; ++i)
-			{
-				List<object> elem = dataList[i] as List<object>;
-				if (elem == null || elem.Count == 0) break;
+    {
+        List<object> dataList = GetDataList(fileName);
+        if (dataList == null)
+        {
+            Debug.Log(GetType().Name + " Load_table_skill_card() : data list is null" + fileName);
+        }
+        else
+        {
+            for (int i = 0, count = dataList.Count; i < count; ++i)
+            {
+                List<object> elem = dataList[i] as List<object>;
+                if (elem == null || elem.Count == 0) break;
 
-				CardData data = CardData.Create(elem);
-				TableDatas.Instance.cardDatas.Add(data.code, data);
-			}
-		}
+                CardData data = CardData.Create(elem);
+                TableDatas.Instance.cardDatas.Add(data.code, data);
+            }
+        }
 #if !UNITY_EDITOR
 		yield break;
 #endif
-	}
+    }
 
 }

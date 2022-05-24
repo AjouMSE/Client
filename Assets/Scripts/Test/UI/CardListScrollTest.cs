e58242a6a3f8cb94822579400870d7c6f25d277a
Onggae22 <hong3883@naver.com>
2022-05-24 23:09:53 +0900

    add. CardListScrollTest Scene
    - add CardListScrollTest scene to test new scroll UI
      (still working on new card scroll UI)

---
+++ Assets/Scripts/Test/UI/CardListScrollTest.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardListScrollTest : MonoBehaviour
+{
+    [SerializeField] private ScrollScript3D scroll3D;
+    [SerializeField] private GameObject[] buttons;
+
+    private RectTransform[] rectTransforms;
+    private Image[] images;
+
+    private Color baseColor, selectedColor;
+
+    private int isOpen;
+
+    public void OnSkillCardClick()
+    {
+        Debug.Log("Skill card click!");
+    }
+
+    public void OnCategoryBtnClick(int idx)
+    {
+        isOpen = idx;
+        StartCoroutine(OpenAndClose());
+    }
+
+    private void Start()
+    {
+        baseColor = new Color(1, 0.87f, 0.75f);
+        selectedColor = new Color(1, 0.75f, 0.36f);
+        rectTransforms = new RectTransform[4];
+        images = new Image[4];
+
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            rectTransforms[i] = buttons[i].GetComponent<RectTransform>();
+            images[i] = buttons[i].GetComponent<Image>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            scroll3D.OpenOrCloseScroll();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            StartCoroutine(OpenAndClose());
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Move Right");
+            scroll3D.MoveMenuRight();
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("Move Right");
+            scroll3D.MoveMenuleft();
+        }
+    }
+    
+    IEnumerator OpenAndClose()
+    {
+        scroll3D.CloseScroll();
+        yield return new WaitForSeconds(0.8f);
+        scroll3D.OpenScroll();
+        int mask = 8;
+        for (int i = 0; i < 4; i++)
+        {
+            Vector3 tmpPos = rectTransforms[i].localPosition;
+            Color tmpColor;
+            if ((isOpen & mask) > 0)
+            {
+                tmpPos.z = 0.03f;
+                tmpColor = selectedColor;
+            }
+            else
+            {
+                tmpPos.z = 0.04f;
+                tmpColor = baseColor;
+            }
+            rectTransforms[i].localPosition = tmpPos;
+            images[i].color = tmpColor;
+            mask >>= 1;
+        }
+    }
+}
